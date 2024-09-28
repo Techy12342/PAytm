0fc76fb4831e286ec9df8df596755af61f878ae4
Techy12342 <subhammoharana84@gmail.com>
2024-09-28 10:12:55 +0530

    Changes:

--- paytm/backend/routes/user.js
+++ paytm/backend/routes/user.js
@@ -13,7 +13,39 @@ const signupschema = zod.object({
     firstname: zod.string(),
     lastname: zod.string()
 })
-router.post("/signup", (req,res) =>{
-    const body = req.body;
+router.post("/signup", async (req, res) => {
+    const { success } = signupBody.safeParse(req.body)
+    if (!success) {
+        return res.status(411).json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const existingUser = await user.findOne({
+        username: req.body.username
+    })
+
+    if (existingUser) {
+        return res.status(411).json({
+            message: "Email already taken/Incorrect inputs"
+        })
+    }
+
+    const user = await user.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    })
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
 })
 module.exports = router
